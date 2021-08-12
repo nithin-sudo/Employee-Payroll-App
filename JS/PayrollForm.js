999ef777dea52ef3267365f4c0b241ea82f966a4
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-12 21:12:13 +0530

    Refactor: Shifted code to another file.

--- JS/PayrollForm.js
+++ JS/PayrollForm.js
@@ -5,80 +5,6 @@ salary.oninput = function() {
     salaryOutput.textContent = salary.value;
 };
 
-class EmployeePayrollData {
-    // Property
-    name;
-    gender;
-    department;
-    salary;
-    startDate;
-    notes;
-
-    constructor(...params) {
-        this.name = params[0];
-        this.gender = params[1];
-        this.department = params[2];
-        this.salary = params[3];
-        this.startDate = params[4];
-        this.notes = params[5];
-    }
-
-
-    get name() {
-        return this._name;
-    }
-    set name(name) {
-        if (regexName.test(name))
-            this._name = name;
-        else
-            throw "Enter Capital First Letter and minimum should be 3 "
-    }
-
-    get gender() {
-        return this._gender;
-    }
-    set gender(gender) {
-        this._gender = gender;
-    }
-
-    get department() {
-        return this._department;
-    }
-    set department(department) {
-        this._department = department;
-    }
-
-    get salary() {
-        return this.salary;
-    }
-    set salary(salary) {
-        this.salary = salary;
-    }
-
-    get startDate() {
-        return this._startDate;
-    }
-    set startDate(startDate) {
-        if (startDate <= new Date())
-            this._startDate = startDate;
-        else
-            throw "Invalid date."
-
-    }
-    get notes() {
-        return this._notes;
-    }
-    set notes(notes) {
-        this._notes = notes;
-    }
-
-    toString() {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = this._startDate === undefined ? "undefined" :
-            new Date(this.startDate).toLocaleDateString("en-US", options);
-        return " Name: " + this.name + "\n Gender: " + this.gender + "\n Department: " + this.department + "\n Salary: " + this.salary + "\n Start Date: " + empDate + "\n Notes: " + this.notes;
-    }
-}
 
 const text = document.querySelector('#name')
 const textError = document.querySelector('.text-error')
@@ -87,7 +13,7 @@ text.addEventListener('input', function() {
     if (nameRegex.test(text.value)) {
         textError.textContent = ""
     } else {
-        textError.textContent = "Name is Incorrect"
+        textError.textContent = "Enter Capital First Letter and minimum should be 3 "
     }
 })
 
