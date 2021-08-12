aa8a62818e1f336d5c4caec045b0dcc93a112ce7
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-12 22:55:30 +0530

    Added: creating object on save.

--- JS/PayrollForm.js
+++ JS/PayrollForm.js
@@ -28,3 +28,76 @@ window.addEventListener( 'DOMContentLoaded', (event) => {
 	   output.textContent = salary.value;
 	});
 });
+
+const save = () =>
+{
+	try
+	{
+		let employeePayrollData = createEmployeePayroll();
+	}
+	catch (e)
+	{
+		return;
+	}
+}
+
+const createEmployeePayroll = () =>
+{
+	let employeePayrollData = new EmployeePayrollData();
+	try
+	{
+		employeePayrollData.name = getInputValueById('#name');
+	}
+	catch (e)
+	{
+		setTextValue('.text-error', e);
+		throw e;
+	}
+	employeePayrollData.profilePic = getSelectedValues('[name=profile]').profilePic();
+	employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+	employeePayrollData.department = getSelectedValues('[name=department]');
+	employeePayrollData.salary = getInputValueById('#salary');
+	employeePayrollData.note = getInputValueById('#notes');
+	let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+	employeePayrollData.date = Date.parse(date);
+	alert(employeePayrollData.toString());
+	return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) =>
+{
+	let allItems = document.querySelectorAll(propertyValue);
+	let selItems = [];
+	allItems.forEach(item => 
+		{
+			if(item.checked) selItems.push(item.value);
+		});
+		return selItems;
+}
+
+/*
+*	1: querySelector is the newer feature.
+*	2: The querySelector method can be used when selecting by element name,
+*		nestingm, or class name.
+*	3: querySelector lets you find elements with rules that can't be
+*		expressed with getElementById.
+*/
+
+const getInputValueById = (id) =>
+{
+	let value = document.querySelector(id).value;
+	return value;
+}
+
+/*
+* 1: getElementById is better supported than querySelector in older versions
+*	of the browsers.
+* 2: The things with getElementById is that it only allows to select an
+*	element by its id.
+*/
+
+const getInputElementValue = (id) =>
+{
+	let value = document.getElementById(id).value;
+	return value;
+}
