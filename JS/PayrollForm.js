b6f59ad854d96d95085c0097b2f949e749c4c59e
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-12 21:31:15 +0530

    Added: Event Listener When Document is Loaded.

--- JS/PayrollForm.js
+++ JS/PayrollForm.js
@@ -1,45 +1,30 @@
-const salary = document.querySelector('#salary');
-const salaryOutput = document.querySelector('.salary-output');
-salaryOutput.textContent = salary.value;
-salary.oninput = function() {
-    salaryOutput.textContent = salary.value;
-};
+window.addEventListener( 'DOMContentLoaded', (event) => {
+	
+	const name = document.querySelector('#name')
+	const textError = document.querySelector('.text-error')
+	name.addEventListener('input',function ()
+	{   
+		if(name.value.length == 0)
+		{
+			textError.textContent = "";
+			return;
+		}
+		try
+		{
+			(new EmployeePayrollData()).name = name.value;
+			textError.textContent = "";
+		}
+		catch (e)
+		{
+			textError.textContent = e;
+		}
+	});
 
-
-const text = document.querySelector('#name')
-const textError = document.querySelector('.text-error')
-text.addEventListener('input', function() {
-    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$")
-    if (nameRegex.test(text.value)) {
-        textError.textContent = ""
-    } else {
-        textError.textContent = "Enter Capital First Letter and minimum should be 3 "
-    }
-})
-
-function save() {
-    try {
-        let name = document.querySelector('#name').value;
-        let namefinal = '';
-        if (name.charAt(0) === name.charAt(0).toLowerCase()) {
-            namefinal = "incorrect";
-        } else {
-            namefinal = name;
-        }
-        let gender = document.querySelector('input[name=gender]:checked').value;
-        let deptList = new Array();
-        let departments = document.querySelectorAll('input[name=department]:checked');
-        for (let i = 0; i < departments.length; i++) {
-            deptList.push(department[i].value);
-        }
-        let salary = document.querySelector('#salary').value;
-        let startDate = document.querySelector('#startDate').value;
-        console.log("startdate", startDate);
-        let notes = document.querySelector('#notes').value;
-        let newEmployee = new EmployeePayrollData(namefinal, gender, departments, salary, startDate, notes);
-        alert(newEmployee);
-    } catch (error) {
-        alert(error);
-    }
-
-}
+	const salary = document.querySelector('#salary');
+	const output = document.querySelector('.salary-output');
+	output.textContent = salary.value;
+	salary.addEventListener('input', function ()
+	{
+	   output.textContent = salary.value;
+	});
+});
