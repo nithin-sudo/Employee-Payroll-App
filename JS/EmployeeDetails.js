cabc0ce924b3b68ec55ef93fcb5f6df3f3e75fb1
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-13 02:11:46 +0530

    Added: Retrieving and Displaying Employee Payroll Data from local storage.

--- JS/EmployeeDetails.js
+++ JS/EmployeeDetails.js
@@ -1,10 +1,23 @@
+let empPayrollList
 window.addEventListener("DOMContentLoaded", (event) =>
 {
+    empPayrollList = getEmployeeDataFromStorage()
+    document.querySelector(".emp-count").textContent = empPayrollList.length
     createInnerHtml();
 });
-  
+
+const getEmployeeDataFromStorage = () =>
+{
+    return localStorage.getItem('EmployeePayrollList') ? 
+    JSON.parse(localStorage.getItem('EmployeePayrollList')) : []
+}
+
 const createInnerHtml = () =>
 {
+    if (empPayrollList.length == 0)
+    {
+        return
+    }
     const headerHtml = `<tr>
     <th></th>
     <th>Name</th>
@@ -14,61 +27,26 @@ const createInnerHtml = () =>
     <th>Start Date</th>
     <th>Actions</th>
     </tr>`;
-    let empPayrollList = createEmployeePayrollJSON()
     let innerHtml = `${headerHtml}`
     for (const empPayrollData of empPayrollList)
     {
         innerHtml = `${innerHtml} 
         <tr>
-            <td><img src="${empPayrollData._profilePic}" alt="" class="profile"></td>
-            <td>${empPayrollData._name}</td>
-            <td>${empPayrollData._gender}</td>
-            <td>${getDeptHtml(empPayrollData._department)}</td>
-            <td>${empPayrollData._salary}</td>
-            <td>${empPayrollData._startDate}</td>
-            <td>
-                <img src="../assets/icons/delete-black-18dp.svg" alt="delete" id="${empPayrollData._id}" onclick="remove(this)">
-                <img src="../assets/icons/create-black-18dp.svg" alt="update" id="${empPayrollData.id}" onclick="update(this)">
-            </td>
+        <td><img src="${empPayrollData._profilePic}" alt="" class="profile"></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
+        <td>
+            <img src="../assets/icons/delete-black-18dp.svg" alt="delete" id="${empPayrollData._id}" onclick="remove(this)">
+            <img src="../assets/icons/create-black-18dp.svg" alt="update" id="${empPayrollData.id}" onclick="update(this)">
+        </td>
         </tr>`;   
     }
     document.querySelector('#table-display').innerHTML = innerHtml
 };
-  
-const createEmployeePayrollJSON = () =>
-{
-    let empPayrollListLocal =
-    [
-        {
-            _name:"Nithin Krishna",
-            _gender:"Male",
-            _department:
-            [
-                "Sales"
-            ],
-            _salary:"300000",
-            _startDate:"12 JUL 2020",
-            _note:"Hey Hi.",
-            _id: new Date().getTime(),
-            _profilePic: "../assets/profile-images/Ellipse -5.png"
-        },
-        {
-            _name:"Khyathi Somepalli",
-            _gender:"Female",
-            _department:
-            [
-                "Sales",
-            ],
-            _salary:"500000",
-            _startDate:"10 august 2019",
-            _note:"Hello there!.",
-            _id: new Date().getTime(),
-            _profilePic: "../assets/profile-images/Ellipse -7.png" 
-        }
-    ]
-    return empPayrollListLocal
-}
-  
+
 const getDeptHtml = (deptList) =>
 {
     let depthtml= "";
