bee9918e226bb76c7405cf378ca0507b58b40113
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-12 20:39:41 +0530

    Added: validation on setter methods.

--- JS/PayrollForm.js
+++ JS/PayrollForm.js
@@ -80,11 +80,11 @@ class EmployeePayrollData {
     }
 }
 
-const name = document.querySelector('#text')
+const text = document.querySelector('#name')
 const textError = document.querySelector('.text-error')
 text.addEventListener('input', function() {
     let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$")
-    if (nameRegex.test(name.value)) {
+    if (nameRegex.test(text.value)) {
         textError.textContent = ""
     } else {
         textError.textContent = "Name is Incorrect"
