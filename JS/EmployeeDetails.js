c3908b6cfb71860878c3260a1d858e12b2d67a19
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-17 21:12:43 +0530

    Added: HTML element for start date.

--- JS/EmployeeDetails.js
+++ JS/EmployeeDetails.js
@@ -37,7 +37,7 @@ const createInnerHtml = () =>
         <td>${empPayrollData._gender}</td>
         <td>${getDeptHtml(empPayrollData._department)}</td>
         <td>${empPayrollData._salary}</td>
-        <td>${empPayrollData._startDate}</td>
+        <td>${stringDate(empPayrollData._startDate)}</td>
         <td>
             <img src="../assets/icons/delete-black-18dp.svg" alt="delete" id="${empPayrollData._id}" onclick="remove(this)">
             <img src="../assets/icons/create-black-18dp.svg" alt="update" id="${empPayrollData.id}" onclick="update(this)">
@@ -55,4 +55,23 @@ const getDeptHtml = (deptList) =>
         depthtml = `${depthtml} <div class="dept-label">${dept}</div>`
     }
     return depthtml
+}
+
+function remove(node)
+{
+    console.log(node.id);
+    let empPayrollData = empPayrollList.find(empData => empData._id == node.id)
+    if (!empPayrollData)
+    {
+        console.log("inside if");
+        return
+    }
+    const index = empPayrollList.map(empData => empData._id).indexOf(empPayrollData._id)
+    console.log(index)
+    console.log(empPayrollList)
+    empPayrollList.splice(index, 1); 
+    console.log(empPayrollList)
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList))
+    document.querySelector(".emp-count").textContent = empPayrollList.length
+    createInnerHtml();
 }
