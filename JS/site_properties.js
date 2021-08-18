4ac8ce890bd8d1a4bccfa6667cec269c9acfdd9b
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-18 19:23:18 +0530

    Modified: code and set local storage to false.

--- JS/site_properties.js
+++ JS/site_properties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local_storage:"true",
+    use_local_storage:"false",
     home_page: "../pages/HomePage.html",
     add_emp_page: "../pages/PayrollForm.html",
     server_url: " http://localhost:3000/EmployeePayrollDB"
