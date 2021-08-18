e228538aea402e4c89c48de21ec6e0986997f802
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-18 19:13:31 +0530

    Added: Methods for name and date.

--- JS/Utility.js
+++ JS/Utility.js
@@ -5,3 +5,22 @@ function stringDate(startDate) {
         ? "undefined"
         : date.toLocaleDateString("en-us", options);
 }
+
+function checkName(name) {
+    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (!nameRegex.test(name)) {
+        throw "Name is incorrect";
+    }
+}
+
+function checkDate(startDate) {
+    let now = new Date()
+    let date = new Date(startDate)
+    if (date > now) {
+        throw 'Date canot be future date'
+    }
+    var diff = Math.abs(now.getTime() - date.getTime())
+    if (diff / (1000 * 60 * 60 * 24) > 30) {
+        throw "Date cannot be beyond 30 days"
+    }
+}
